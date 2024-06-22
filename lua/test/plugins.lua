4167633c8ee95203af33830ca424b487b290a49d
Matteo Lazzarin <mattyla285@gmail.com>
2024-06-22 09:12:04 +0200

    My first config

---
+++ lua/test/plugins.lua
@@ -0,0 +1,156 @@
+-- Automatically run: PackerCompile
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = vim.api.nvim_create_augroup("PACKER", { clear = true }),
+    pattern = "plugins.lua",
+    command = "source <afile> | PackerCompile",
+})
+
+return require("packer").startup(function(use)
+    -- Packer
+    use("wbthomason/packer.nvim")
+
+    -- Common utilities
+	  use("nvim-lua/plenary.nvim")
+
+	  -- Icons
+	  use("nvim-tree/nvim-web-devicons")
+
+    --Colorschema
+    use("rebelot/kanagawa.nvim")
+    
+    --StatusLine
+    use({
+		    "nvim-lualine/lualine.nvim",
+		    event = "BufEnter",
+		    config = function()
+			            require("test.configs.lualine")
+		            end,
+		    requires = { "nvim-web-devicons" },
+	  })
+
+    -- Treesitter
+	  use({
+		    "nvim-treesitter/nvim-treesitter",
+		    run = function()
+			          require("nvim-treesitter.install").update({ with_sync = true })
+		    end,
+		    config = function()
+			          require("test.configs.treesitter")
+		    end,
+	  })
+
+    use({ "windwp/nvim-ts-autotag",
+          after = "nvim-treesitter" 
+    })
+    
+    -- Telescope
+	  use({
+		    "nvim-telescope/telescope.nvim",
+		    tag = "0.1.8",
+		    requires = { { "nvim-lua/plenary.nvim" } },
+	  })
+	
+    -- cmp: Autocomplete
+	  use({
+		  "hrsh7th/nvim-cmp",
+		  event = "InsertEnter",
+		  config = function()
+			      require("test.configs.cmp")
+		  end,
+	  })
+
+	  use("hrsh7th/cmp-nvim-lsp")
+
+	  use({ "hrsh7th/cmp-path", after = "nvim-cmp" })
+
+	  use({ "hrsh7th/cmp-buffer", after = "nvim-cmp" })
+  	
+    -- LSP
+	  use({
+		    "neovim/nvim-lspconfig",
+		    config = function()
+			      require("test.configs.lsp")
+		    end,
+	  })
+    
+    use("onsails/lspkind-nvim")
+
+    -- LSP diagnostics, code actions, and more via Lua.
+	  use({
+		    "jose-elias-alvarez/null-ls.nvim",
+		    config = function()
+			              require("test.configs.null-ls")
+		              end,
+        requires = { "nvim-lua/plenary.nvim" },
+	  })
+
+  	-- Mason: Portable package manager
+	  use({
+		    "williamboman/mason.nvim",
+		    config = function()
+			            require("mason").setup()
+		             end,
+	  })
+
+	  use({
+		    "williamboman/mason-lspconfig.nvim",
+		    config = function()
+			              require("test.configs.mason-lsp")
+		            end,
+		    after = "mason.nvim",
+	  })
+    
+    -- File manager
+	  use({
+		    "nvim-neo-tree/neo-tree.nvim",
+		    branch = "v3.x",
+		    requires = {
+			      "nvim-lua/plenary.nvim",
+			      "nvim-tree/nvim-web-devicons",
+			      "MunifTanjim/nui.nvim",
+		    },
+	  })
+    
+    -- Show colors
+	  use({
+		    "norcalli/nvim-colorizer.lua",
+		    config = function()
+			              require("colorizer").setup({ "*" })
+		            end,
+	  })
+
+    -- Terminal
+	  use({
+		    "akinsho/toggleterm.nvim",
+		    tag = "*",
+		    config = function()
+			            require("test.configs.toggleterm")
+		            end,
+	  })
+
+    -- Git
+	  use({
+		    "lewis6991/gitsigns.nvim",
+		    config = function()
+			            require("test.configs.gitsigns")
+		            end,
+	  })
+
+    -- Markdown Preview
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function()
+                vim.fn["mkdp#util#install"]()
+              end,
+    })
+
+    -- autopairs
+    use({
+        "windwp/nvim-autopairs",
+        config = function()
+                    require("test.configs.autopairs")
+                 end,
+    })
+
+
+end)
