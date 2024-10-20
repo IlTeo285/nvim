863a86529dfc91c8c5c4b372763a77eb47b16a6c
Matteo Lazzarin <mattyla285@gmail.com>
2024-10-20 09:17:13 +0200

    fix kanagawa color scheme

--- init.lua
+++ init.lua
@@ -1096,16 +1096,21 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    init = function()
+      vim.cmd.colorscheme 'kanagawa'
     end,
   },
 
